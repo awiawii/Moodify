699bf53f171abb4d33bd8179ea7b04bf5c74c06a
mbprayoga <entdecker000@gmail.com>
2024-05-24 03:16:24 +0700

    module

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
+// routes/auth.js
+
 const express = require('express');
-const authController = require('../controllers/authController');
+const { signin, register, logout, signinWithGoogle } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.get("/accounts", authController.showAccounts);
-router.post("/accounts/add", authController.addAcount);
+router.post('/login', signin);
+router.post('/register', register);
+router.post('/login/google', signinWithGoogle); 
+router.post('/logout', logout);
 
-module.exports = router;
+module.exports = router;
