4ea8f8aba8a7aa014eb9a5ed4b7fd31ed1df89bc
mbprayoga <102417378+mbprayoga@users.noreply.github.com>
2024-06-15 18:57:17 +0700

    Update copingCategory.js

--- routes/copingCategory.js
+++ routes/copingCategory.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const copingCategoryController = require('../controllers/copingCategoryController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Middleware for authentication
-router.use(authMiddleware);
-
 // Route to get mood coping data
 router.get('/type/music/', copingCategoryController.getMoodCopingDataMusic);
 router.get('/type/podcast/', copingCategoryController.getMoodCopingDataPodcast);
